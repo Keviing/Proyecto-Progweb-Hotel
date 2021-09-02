0f87cc725951638aa635f28a16c2b3270e9851b3
Keviing <gualpakevin@gmail.com>
2021-09-02 12:46:38 -0500

    backend and frontend

---
+++ web-frontend-hotel/js/main.js
@@ -0,0 +1,11 @@
+var menu = '<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Ninth navbar example"> <div class="container-xl"> <a class="navbar-brand" href="index.html">Hotel</a> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button> <div class="collapse navbar-collapse" id="navbarsExample07XL"> <ul class="navbar-nav me-auto mb-2 mb-lg-0"> <li class="nav-item"> <a class="nav-link active" aria-current="page" href="index.html">Inicio</a> </li> <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-bs-toggle="dropdown" aria-expanded="false">Invernadero</a> <ul class="dropdown-menu" aria-labelledby="dropdown07XL"> <li><a class="dropdown-item" href="servicio.html">Reservar</a></li> <li><a class="dropdown-item" href="invernadero-list.html">Listado</a></li> </ul> </li> <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-bs-toggle="dropdown" aria-expanded="false">Variedad</a> <ul class="dropdown-menu" aria-labelledby="dropdown07XL"> <li><a class="dropdown-item" href="variedad-form.html">Nueva</a></li> <li><a class="dropdown-item" href="variedad-list.html">Listado</a></li> </ul> </li> </ul> </div> </div> </nav>';
+var footer = '<ul class="nav justify-content-center border-bottom pb-3 mb-3"> <li class="nav-item"><a href="index.html" class="nav-link px-2 text-muted">Inicio</a></li> <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li> <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li> <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li> <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li> </ul> <p class="text-center text-muted">&copy; 2021 ISOW, Carrera de Ingenieria de Software</p>';
+function loadMenu(){
+    $('header').html(menu);
+    $('footer').html(footer);
+}
+
+$(function() {
+    console.log("Pagina lista");
+    loadMenu();
+});
